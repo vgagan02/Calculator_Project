6621320c2f2b16c0f1bd95a79646423704b674c7
vgagan02 <vgagan273@gmail.com>
2024-07-14 23:58:00 +0530

    nav bar and main buttons

---
+++ calculator_proj/src/Main..jsx
@@ -0,0 +1,23 @@
+
+function Main(){
+    
+    return(
+        <div className="mainbody">
+            <button>+</button>
+            <button>-</button>
+            <button>AC</button>
+            <button>9</button>
+            <button>8</button>
+            <button>7</button>
+            <button>6</button>
+            <button>5</button>
+            <button>4</button>
+            <button>3</button>
+            <button>2</button>
+            <button>1</button>
+            <button>=</button>
+        </div>
+    );
+}
+
+export default Main
---
+++ calculator_proj/src/Nav.jsx
@@ -0,0 +1,14 @@
+
+function Nav(){
+    return(
+        <div className="topcard">
+            <div className="heading"><h1>Calculator Website</h1></div>
+            <div className="options">
+                <h2 className="option" >About</h2>
+                <h2 className="option">Calculator</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
